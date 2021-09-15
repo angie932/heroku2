4fcede85b16b3d3b4ea03596cb16c5d8e7e5ea3a
anonimo223 <anguielugo44@gmail.com>
2021-09-15 18:13:20 -0500

    Se sube todo el proyecto.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/estilos6.css">
-    <title>Document</title>
+    <title>Documento</title>
 </head>
 <body>
     <section class="portafolio">
